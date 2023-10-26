ee9f6c49f68437d37a304c46c81d0a212ad3d956
katiacreator <kcygibson@gmail.com>
2023-10-26 15:07:20 -0400

    added getweather data functionality via proxy server

--- index.js
+++ index.js
@@ -1,10 +1,26 @@
 const express = require('express')
 const cors = require('cors')
+const rateLimit = require('express-rate-limit')
 require('dotenv').config()
 
 const PORT = process.env.PORT || 5000
 
 const app = express()
+
+// rate limiting
+const limiter = rateLimit({
+    windowMS: 10 * 60 * 1000,//10 mins
+    max: 5 
+})
+
+app.use(limiter)
+app.set('trust proxy', 1)
+
+// set static folder
+app.use(express.static('public'))
+
+// Routes
+app.use('/api', require('./routes'))
 
 // enable cors
 app.use(cors())
